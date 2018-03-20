7f8dcf4c1a83587fdf9e1c7549f1ff7544062c78
hwral <hwral@DESKTOP-JF2I7CT>
2018-03-21 00:52:46 +0900

    최초 1장의 Hello World 테스트

---
+++ src/main/java/com/acorn/makeitsimple/MakeitsimpleApplication.java
@@ -0,0 +1,12 @@
+package com.acorn.makeitsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeitsimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeitsimpleApplication.class, args);
+	}
+}
