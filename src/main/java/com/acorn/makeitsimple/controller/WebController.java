b17ddde85438068c4caeb486c9d536526774a077
hwral <hwral@DESKTOP-JF2I7CT>
2018-03-21 02:42:00 +0900

    Step2-1 ACME템플릿 복사 및 WebController 작성

---
+++ src/main/java/com/acorn/makeitsimple/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.acorn.makeitsimple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "index";
+	}
+}
